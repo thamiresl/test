7b81da3a146338d20d1bac8200cec84eabc14fb6
Thamires Lourenço <thami.lourenco@outlook.com>
2022-01-17 17:03:55 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-// add your implementation here
-
-module.exports = average;
+const average = (array) => {
+  let soma = 0;
+  let resultado = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+  if (typeof (array[index]) !== 'number') {
+    return undefined;
+  }
+  soma += array[index];
+  }
+  resultado = Math.round(soma / array.length);
+  return resultado;
+  };
+  module.exports = average;
